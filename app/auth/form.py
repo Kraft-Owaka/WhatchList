111a321526cee0bc35e1384854e5136d3b664b79
kraft-owaka <owakakraft@gmail.com>
2020-02-07 15:32:34 +0300

    creating a registration for inside the auth/form

---
+++ app/auth/form.py
@@ -0,0 +1,6 @@
+from . import views,forms
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
