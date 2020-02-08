228454c6f9f79bca7bd248a667e529b9a29a1a36
kraft-owaka <owakakraft@gmail.com>
2020-02-08 05:33:38 +0300

    created  template file  for our profile where we display our username, bio and profile picture of the User.

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from ..request import get_movies,get_movie,search_movie
 from .form import ReviewForm
-from ..models import Review
+from ..models import Review, User
 from flask_login import login_required
 
 
@@ -29,6 +29,16 @@ def index():
         return redirect(url_for('.search',movie_name=search_movie))
     else:
          return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
 
 
 @main.route('/movie/<int:movie_id>')
